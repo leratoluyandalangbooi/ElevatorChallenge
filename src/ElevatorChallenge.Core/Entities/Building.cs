10c476beecaa990befc618b77d96392ce1a7bb25
LuyaLerato <luyandalangbooi@gmail.com>
2024-08-26 23:17:12 +0200

    Added Building, Floor domain objects

---
+++ src/ElevatorChallenge.Core/Entities/Building.cs
@@ -0,0 +1,22 @@
+﻿namespace ElevatorChallenge.Core.Entities;
+
+public class Building
+{
+    public List<Floor> Floors { get; }
+    public List<Elevator> Elevators { get; }
+
+    public Building()
+    {
+        
+    }
+
+    public Floor GetFloor(int floorNumber)
+    {
+        return Floors.FirstOrDefault(f => f.FloorNumber == floorNumber);
+    }
+
+    public Elevator GetElevator(int elevatorId)
+    {
+        return Elevators.FirstOrDefault(e => e.Id == elevatorId);
+    }
+}
---
+++ src/ElevatorChallenge.Core/Entities/Floor.cs
@@ -0,0 +1,20 @@
+﻿namespace ElevatorChallenge.Core.Entities;
+
+public class Floor
+{
+    public int FloorNumber { get; }
+    public Queue<Passenger> PassengersQueue { get; }
+    public Dictionary<Direction, bool> ElevatorCallButtons { get; }
+
+    public Floor(int floorNumber)
+    {
+        FloorNumber = floorNumber;
+        PassengersQueue = new Queue<Passenger>();
+        ElevatorCallButtons = new Dictionary<Direction, bool>
+        {
+            { Direction.Up, false },
+            { Direction.Down, false }
+        };
+    }
+
+}
