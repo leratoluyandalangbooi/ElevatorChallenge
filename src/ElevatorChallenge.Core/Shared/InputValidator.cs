e80226171bf2bee43fcd1f925dd15d213882aeec
LuyaLerato <luyandalangbooi@gmail.com>
2024-09-27 14:37:32 +0200

    Add input validator (wip) - set single/common validator point - integrate it on input validator on elevatorService

---
+++ src/ElevatorChallenge.Core/Shared/InputValidator.cs
@@ -0,0 +1,28 @@
+﻿namespace ElevatorChallenge.Core.Shared;
+
+public static class InputValidator
+{
+    public static void ValidateFloorNumber(int floorNumber, int maxFloors)
+    {
+        if (floorNumber < 1 || floorNumber > maxFloors)
+        {
+            throw new ArgumentOutOfRangeException(nameof(floorNumber), $"Floor number must be between 1 and {maxFloors}");
+        }
+    }
+
+    public static void ValidateElevatorId(int elevatorId, int maxElevators)
+    {
+        if (elevatorId < 1 || elevatorId > maxElevators)
+        {
+            throw new ArgumentOutOfRangeException(nameof(elevatorId), $"Elevator ID must be between 1 and {maxElevators}");
+        }
+    }
+
+    public static void ValidateDirection(Direction direction)
+    {
+        if (!Enum.IsDefined(typeof(Direction), direction))
+        {
+            throw new ArgumentException("Invalid direction", nameof(direction));
+        }
+    }
+}
